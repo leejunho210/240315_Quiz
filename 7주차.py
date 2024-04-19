b08986341a311ef5725edff673b5b479d0590734
leejunho210 <163738398+leejunho210@users.noreply.github.com>
2024-04-19 15:49:58 +0900

    Add files via upload

---
+++ 7주차.py
@@ -0,0 +1,11 @@
+numbers=[[1,2,3,],
+         [4,5,6,],
+         [7,8,9]]
+
+results = []
+
+for row in numbers:
+    for numbers in row:
+        if numbers % 2 == 0:
+            results.append(numbers)
+print("짝수", results)
